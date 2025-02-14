6336a502d8d5b1c6c66ba0d447e0e279ada50134
유정주 <dbwjdwn9216@gmail.com>
2025-02-14 22:35:18 +0900

    프로젝트 생성

---
+++ KeepRolling/KeepRolling/KeepRollingApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeepRollingApp.swift
+//  KeepRolling
+//
+//  Created by 유정주 on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct KeepRollingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
